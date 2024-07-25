919afb14fce154b1472729f1f2543611c3a87aa2
Wissam-El-Labban <wissamlabban2015@gmail.com>
2024-07-25 17:34:19 -0400

    added initial permuataion function

--- Encrypt.py
+++ Encrypt.py
@@ -32,12 +32,31 @@ def blocks_to_text(blocks):
     text = binary_to_text(binary_string)
     return text
 
+def initial_permutation(block):
+    ip_table = [
+        58, 50, 42, 34, 26, 18, 10,  2,
+        60, 52, 44, 36, 28, 20, 12,  4,
+        62, 54, 46, 38, 30, 22, 14,  6,
+        64, 56, 48, 40, 32, 24, 16,  8,
+        57, 49, 41, 33, 25, 17,  9,  1,
+        59, 51, 43, 35, 27, 19, 11,  3,
+        61, 53, 45, 37, 29, 21, 13,  5,
+        63, 55, 47, 39, 31, 23, 15,  7
+    ]
+    if len(block) != 64 or not all (bit in '01' for bit in block):
+        raise ValueError("Invalid block")
+    
+    permuted_block = [block[i-1] for i in ip_table]
+    return permuted_block
 
-plaintext = "This is a test!"
-blocks = text_to_64bit_blocks(plaintext)
-for i , block in enumerate(blocks):
-    print(f"block {i+1}: {block}")
-
-print(blocks_to_text(blocks))
+if __name__ == '__main__':
+    plaintext = "This is a test!"
+    blocks = text_to_64bit_blocks(plaintext)
+    for i , block in enumerate(blocks):
+        print(f"block {i+1}: {block}")
+        print(initial_permutation(block))
+    
+    print(blocks_to_text(blocks))
+    
     
 
